e491a88c8e6ed4c8716413473747cc262be1fcf3
Huỳnh Nguyên Phúc <phucthcsmyloc@gmail.com>
2024-06-11 11:55:53 +0700

    Fix issues in drawing points

--- app/src/main/java/com/example/opengl/BarycentricRender.java
+++ app/src/main/java/com/example/opengl/BarycentricRender.java
@@ -112,6 +112,10 @@ public class BarycentricRender extends MeshRender {
 
     @Override
     protected void drawPoints(float pointSize) {
+        if (mPointBuffer.capacity() == 0) {
+            return;
+        }
+
         super.drawPoints(pointSize);
 
         GLES30.glUniform4fv(mColorHandle, 0, Color.BLUE, 0);
